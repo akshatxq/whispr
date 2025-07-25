21ac6234e8a0166676ec09c88a5bc289b90a4fff
akshatxq <akshatxq@gmail.com>
2025-07-25 13:21:10 +0530

    online status show recovery

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-  import.meta.env.MODE === "development"
-    ? "https://whispr-g444.onrender.com/api"
-    : "/api", // optional if deploying frontend later
-
+  baseURL: "https://whispr-g444.onrender.com/api",
   withCredentials: true,
 });
